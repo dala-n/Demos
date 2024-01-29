f03c08273e2826a991450703b83c81e7d5d0792f
dala-n <danielhollberg@vt.edu>
2024-01-29 14:19:25 -0500

    Add stuff

--- web_server_demos/app.py
+++ web_server_demos/app.py
@@ -1,3 +1,8 @@
+# Import Flask framework
+# Creating app
+# Setting the route the "main directory"
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,6 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World'
+
+@app.route('/home')
+def home():
+    return 'Wow new page!'
 
 if __name__ == '__main__':
     app.run(debug=True, host = '0.0.0.0')
